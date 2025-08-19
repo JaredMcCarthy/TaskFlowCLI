927836e76d45adda2159e657e429cfede8d01d9a
Jared Alexander Lopez McCarthy <jared.lopezmccarthy05@gmail.com>
2025-08-18 21:14:53 -0600

    se encerro el bucle while en una class para uisar en tasks.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 
-while True:
-    print("\n--- Menú ---")
-    print("1 - Agregar una tarea nueva")
+# hacer un menu para probar anandir tareas
+# el objetivo es que el programa me deje agregar tareas y que se guarde en una lista de memoria
 
-    opcion = input("Porfavor seleccione una opcion.\n")
 
-    try:
-        if opcion == '1':
-            tarea = input("Ingrese el nombre de la tarea.\n")
-            category = input("Ingrese la categoria la categoria\n")
-            prioridad = input("Ingrese la prioridad de la tarea.\n")
-            estado = input("Ingrese el estado actual.\n")
-    except ValueError:
-        print("Ingresa valores que sean aceptados.")
-    break
+class menu_inicio:
+    while True:
+        print("\n--- Menú ---")
+        print("1 - Agregar una tarea nueva")
+
+        opcion = input("Porfavor seleccione una opcion.\n")
+
+        try:
+            if opcion == '1':
+                tarea = input("Ingrese el nombre de la tarea.\n")
+                category = input("Ingrese la categoria la categoria\n")
+                prioridad = input("Ingrese la prioridad de la tarea.\n")
+                estado = input("Ingrese el estado actual.\n")
+                confirmacion = input("Desea confirmar las tareas? S/N\n")
+        except ValueError:
+            print("Ingresa valores que sean aceptados.")
+        break
+
+
+

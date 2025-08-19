fc7020ea119c829b27debf91ae2fd2f0798118d8
Jared Alexander Lopez McCarthy <jared.lopezmccarthy05@gmail.com>
2025-08-18 21:54:06 -0600

    ni idea que edite o borre aqui solo subo por si acaso

--- tasks.py
+++ tasks.py
@@ -1,10 +1,19 @@
 
 
-lista_global = []
+import main
+
+tareas = {
+    "nombre": "Jared",
+    "categoria": "Comida",
+    "prioridad": "Alta",
+    "Estado": "En curso",
+    "confirmacion": "S/N"
+}
 
 # guarder las tareas en una lista global por hora
 #crear la funcion agragr tareas DONE
 
 
-def agregar_tareas(main):
-    pass
+def agregar_tareas():
+    global tareas
+

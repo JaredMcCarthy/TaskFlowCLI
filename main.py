121a5e0ea43b7d85db9852652b45aecc2c38d437
Jared Alexander Lopez McCarthy <jared.lopezmccarthy05@gmail.com>
2025-08-18 21:53:25 -0600

    se guardaron los datos pero en archivo main, se muestran sin errores pero falta justar

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-
 # hacer un menu para probar anandir tareas
 # el objetivo es que el programa me deje agregar tareas y que se guarde en una lista de memoria
 
+lista_global = []
 
-class menu_inicio:
+
+class clase_principal:
+    def __init__(self, tarea, category, prioridad, estado, ):
+        self.tarea = tarea
+        self.category = category
+        self.prioridad = prioridad
+        self.estado = estado
+
+
+def menu_inicio():
     while True:
         print("\n--- Menú ---")
         print("1 - Agregar una tarea nueva")
@@ -17,9 +26,16 @@ class menu_inicio:
                 prioridad = input("Ingrese la prioridad de la tarea.\n")
                 estado = input("Ingrese el estado actual.\n")
                 confirmacion = input("Desea confirmar las tareas? S/N\n")
+                if confirmacion == "S":
+                    objeto = clase_principal(tarea, category, prioridad, estado)
+                    lista_global.append(objeto)
+                    print("Los datos se han agreagdo a la lista.")
+                    break
         except ValueError:
             print("Ingresa valores que sean aceptados.")
-        break
+            break
 
+menu_inicio()
 
-
+for obj in lista_global:
+    print(f"Tarea: {obj.tarea}, Categotia: {obj.category}, prioridad: {obj.prioridad}, estado: {obj.estado}.")

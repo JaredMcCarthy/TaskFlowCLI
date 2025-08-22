f19a3174e9709a613ae52741d913f52b096bf42d
Jared Alexander Lopez McCarthy <jared.lopezmccarthy05@gmail.com>
2025-08-21 22:02:31 -0600

    logica de agregar y listar tareas pero con falla al momento de hacer break pero si funcion

--- tasks.py
+++ tasks.py
@@ -1,28 +1,36 @@
 
-from main import menu_inicio
+
+tareas = []
+
+def agregar_tareas(tarea):
+    tareas.append(tareas)
+    print(f"La tarea {tarea} se agrego a la lista.")
 
 
-lista_global = {
-    "nombre": "Jared",
-    "categoria": "Comida",
-    "prioridad": "Alta",
-    "Estado": "En curso",
-    "confirmacion": "S/N"
-}
-
-# guarder las tareas en una lista global por hora
-#crear la funcion agragr tareas DONE
-
-
-def agregar_tareas(datos):
-    global lista_global
-
-
-def listar_tareas(menu_inicio):
-    datos = menu_inicio(object)
-    print(datos)
+def listar_tareas():
+    if not tareas:
+        print("No ha sido agregada a la lista.")
+    else:
+        print("-----Lista de tareas-----")
+        for i, tarea in enumerate(tareas):
+            print(f"{i + 1}. {tarea}")
+            break
 
 
 
-
-
+# import main
+#
+#
+# # import main
+#
+#
+# # guarder las tareas en una lista global por hora
+# #crear la funcion agragr tareas DONE
+#
+#
+# def agregar_tareas():
+#     global lista_global
+#
+#
+# def listar_tareas():
+#     pass

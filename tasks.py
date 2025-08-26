a28df5663e6828cd775f319210d556183e10d20c
Jared Alexander Lopez McCarthy <jared.lopezmccarthy05@gmail.com>
2025-08-25 21:20:12 -0600

    agregando logica de la fase 4 de completadas y numeros indices al usuario

--- tasks.py
+++ tasks.py
@@ -1,11 +1,19 @@
 
+import random
 
 tareas = []
+random_number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+random_letter = ["A", "E", "I", "O", "U"]
 
 def agregar_tareas(tarea):
     tareas.append(tareas)
     print(f"La tarea {tarea} se agrego a la lista.")
+    print(f"La categoria {nueva_categoria} se agrego a la lista.")
 
+def tareas_completadas():
+    print("Que tareas estan completas?")
+    print("Inserte el codigo de tarea")   #aqui la logica hay que trabajarla para que cuando el usuario quiera senalar que esta completa, se marque completa
+                                        #y luego se marque como completa en la lista global o crear otra no se.
 
 def listar_tareas():
     if not tareas:

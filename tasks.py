be9eaa2de9c507425e73119a4ef1a995749f5e12
Jared Alexander Lopez McCarthy <jared.lopezmccarthy05@gmail.com>
2025-09-03 20:39:15 -0600

    trabajando en def tareas_completadas de 0 para ver si funciona la logica con otras funciones al usuario

--- tasks.py
+++ tasks.py
@@ -15,9 +15,20 @@ def agregar_tareas(agregdos):
     tareas_global.append(listar_tareas)
 
 
-def tareas_completadas():
-    print("Que tareas estan completas?")
-    print("Inserte el codigo de tarea")
+def tareas_completadas(confirmacion=True):
+    QA = input("Ingrese el codigo de la tarea completada? \n")
+    print(QA)
+
+    while confirmacion == True:
+        ingreso_confirmacion = input("Quiere confirmar que este es el codigo?")
+
+        try:
+            if ingreso_confirmacion == "S":
+                pass
+        except ValueError:
+            pass
+        # Tomar codigo de tareas agregadas nada mas.
+        #luego de eso que el usuario agregue done o no.
 
 
 def listar_tareas(nueva_tarea, nueva_categoria, nueva_prioridad, nuevo_estado):
@@ -25,15 +36,9 @@ def listar_tareas(nueva_tarea, nueva_categoria, nueva_prioridad, nuevo_estado):
         print("No se han registrado tareas hasta el momento.")
     else:
         for i in enumerate(tareas_global):
-            print( {
+            print({
                 "Tarea": {nueva_tarea},
                 "Categoria": {nueva_categoria},
                 "Prioridad": {nueva_prioridad},
                 "Estado": {nuevo_estado}
             })
-
-
-
-
-
-

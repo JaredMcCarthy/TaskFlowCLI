af1d49f1a52a1fe2e547cc348df38bb8d224b604
Jared Alexander Lopez McCarthy <jared.lopezmccarthy05@gmail.com>
2025-09-10 21:38:04 -0600

    agregando funcionalidad del menu y importando funciones de otro archivo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from tasks import agregar_tareas
-
+from tasks import agregar_tareas, listar_tareas, tareas_completadas
+import sys #exportando funcion para salir del programa
 
 def mostra_menu():
     print("\n--- Menú Principal ---")
@@ -9,13 +9,24 @@ def mostra_menu():
 
 
 def main():
+
+    tareas_guardadas = []
+
     while True:
         mostra_menu()
         opcion = input("Seleccionar unas de las opciones \n")
 
         if opcion == '1':
-            agregar_tareas()
-            print("Haz seleccionado la opcion 1 ;)")
+            agregar_tareas(tareas_guardadas)
+        elif opcion == '2':
+            listar_tareas(tareas_guardadas)
+        elif opcion == '3':
+            tareas_completadas(listar_tareas)
+        elif opcion == '4':
+            print("Saliendo del programa...")
+            sys.exit()
+
+
 
 
 if __name__ == "__main__":

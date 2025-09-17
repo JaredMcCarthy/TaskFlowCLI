27a5958a5b6b52b42f19e4925f465cf9ff336f72
Jared Alexander Lopez McCarthy <jared.lopezmccarthy05@gmail.com>
2025-09-16 21:44:18 -0600

    pequenos cambios de sintax con el menu

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tasks import agregar_tareas, listar_tareas, tareas_completadas
 import sys #exportando funcion para salir del programa
 
+import tasks
+
 def mostra_menu():
     print("\n--- Menú Principal ---")
     print("1 - Agregar tareas")
@@ -20,11 +22,11 @@ def main():
             agregar_tareas(tareas_guardadas)
         elif opcion == '2':
             listar_tareas(tareas_guardadas)
-        elif opcion == '3':
+        elif opcion == '0':
             tareas_completadas(listar_tareas)
-        elif opcion == '4':
+        elif opcion == '3':
             print("Saliendo del programa...")
-            sys.exit()
+            sys.exit()  #sale del programa
 
 
 

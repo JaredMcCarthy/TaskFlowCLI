1718d233aabda3a8703a56c4cda57ec55aacf7b4
Jared Alexander Lopez McCarthy <jared.lopezmccarthy05@gmail.com>
2025-10-21 18:36:35 -0600

    agregando nuevo file para guardar codigo de pruebas por si acaso en el futuro

---
+++ deshuasadero.py
@@ -0,0 +1,56 @@
+#Codigo de aqui para abajo es para nada mas por si acaso y pruebas que hicimos antes de hacerlo funcionar.
+
+
+
+#ahora vamos a usar esta nadamas para subir a git y el otro a programar.
+
+# lista_secundaria.append(n_tarea)
+# lista_secundaria.append(c_tarea)
+# lista_secundaria.append(p_tarea)
+# lista_secundaria.append(e_tarea)
+
+
+# if not lista_tarea:
+#     print("No hay datos guardados.")
+# else:
+#     for indice, (clave, valor) in enumerate(lista_tareas.items()):
+#         print(f" {indice + 1}) {clave}: {valor}")      #de esta manera asiganos una llave y lo que el usuario ingrese al momento de imprimir el resultado
+#
+
+
+# nombre_tarea = input("Cual es la tarea que quiere agendar? \n")
+# categoria_tarea = input("Cual es la categoria de la tarea? \n")
+# prioridad_tarea = input("Agregue la prioridad del 1 - 10 \n")
+# estado_tarea = input("Cual es el estado actual de la tarea? C/N \n")
+# datos_guardados.append(nombre_tarea)
+# datos_guardados.append(categoria_tarea)
+# datos_guardados.append(prioridad_tarea)
+# datos_guardados.append(estado_tarea)
+
+# tarea_actual = {
+#     datos_guardados['Tarea'] =  nombre_tarea,
+#     "Categoria": categoria_tarea,
+#     "Prioridad": prioridad_tarea,
+#     "Estado": estado_tarea
+# }
+
+# datos_guardados.append(tarea_actual)
+
+#este codigo lo dejaremos de backup por si acaso, sigue funcional el otro
+# # Aqui me guarda las tareas en la lista memoria y luego las muestra en listar_tareas
+# lista_tareas.append(nombre_tarea)
+# lista_tareas.append(categoria_tarea)
+# lista_tareas.append(prioridad_tarea)
+# lista_tareas.append(estado_tarea)
+
+# print(f" Nombre: {tarea['Tarea']}")
+# print(f"Categoria: {tarea['Categoria']}")
+# print(f"Estado: {tarea['Estado']}")
+# print("-" * 20)  #separador visual
+
+# print("\n--- Estado de las tareas ---")
+# if not datos_guardados:
+#     print("No hay tareas por mostrar.")
+#     return
+# for i, tareas in enumerate(datos_guardados):
+#     print(f"{i + 1}. {tareas}") #me muestra todas las tareas

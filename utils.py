5ed863d4f179c9a1115de161bbfc1dc7d92ec259
jared <jared.lopezmccarthy05@gmail.com>
2026-01-15 22:16:53 -0600

    avance en la funcion dos donde se supone que debe cambiar la fecha completado cuando el usuario cambia de estado

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
-
 from tasks import datos_globales_dicc
-
+import datetime
 from tasks import listar_tareas
+
+time = datetime.datetime.now()
+hora_completed = time.strftime("%H:%M:%S")
 
 # ANTES DE ESCRIBIR CODIGO EN ESTE ARCHIVO PIENSA:
 # Y TMB DECILO EN VOS ALTA JARED
@@ -13,38 +15,37 @@ from tasks import listar_tareas
 
 
 def cambiar_estado():
-	confirmacion = input("Desea cambiar el estado de una tarea? \n")
+    confirmacion = input("Desea cambiar el estado de una tarea? \n")
 
+    listar_tareas()
+    req_id = input("Ingrese el numero de tarea que quiere cambiar \n")
+    encontrado = False
 
-	listar_tareas()
-	req_id = input("Ingrese el numero de tarea que quiere cambiar \n")
-	encontrado = False
-
-	#lo mejor seria recorrer por el elemento directamente
-	for tarea in datos_globales_dicc:
-		if tarea['ID'] == req_id:
-			encontrado = True
-			if tarea['estado'] == 'si':
-				tarea['estado'] = 'no'
-			else:
-				tarea['estado'] = 'si'
-			print('Estado cambiado con exito.')
-			break
-	if not encontrado:
-		print("No se encontro la tarea.")
+    #lo mejor seria recorrer por el elemento directamente
+    for tarea in datos_globales_dicc:
+        if tarea['ID'] == req_id:
+            encontrado = True
+            if tarea['estado'] == 'si':
+                tarea['estado'] = 'no'
+            else:
+                tarea['estado'] = 'si'
+            print('Estado cambiado con exito.')
+            break
+    if not encontrado:
+        print("No se encontro la tarea.")
 
 
 def eliminar_tareas():
-	listar_tareas()
-	req_id2 = int(input('Ingrese el numero de tarea que quiere eliminar'))
-	encontrado2 = False
+    listar_tareas()
+    req_id2 = int(input('Ingrese el numero de tarea que quiere eliminar'))
+    encontrado2 = False
 
-	for tarea2 in datos_globales_dicc:
-		if tarea2['ID'] == req_id2:
-			encontrado2 = True
-			datos_globales_dicc.remove(tarea2) #mejor porque ya se el ID
-			print("Tarea eliminada con exito.")
-			break
+    for tarea2 in datos_globales_dicc:
+        if tarea2['ID'] == req_id2:
+            encontrado2 = True
+            datos_globales_dicc.remove(tarea2)  #mejor porque ya se el ID
+            print("Tarea eliminada con exito.")
+            break
 
-	if not encontrado2:
-		print("No hay ningun numero con esa tarea.")
+    if not encontrado2:
+        print("No hay ningun numero con esa tarea.")

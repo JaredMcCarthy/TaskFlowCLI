8df0416f0aec325c2b0948c1c31396809209d5b6
jared <jared.lopezmccarthy05@gmail.com>
2026-05-12 13:26:16 -0600

    se agregaron clases conectadas con tasks para mejor manejo

--- tarea.py
+++ tarea.py
@@ -2,24 +2,58 @@
 import uuid
 import datetime
 
-today = datetime.datetime.now()
+from data import guardar_tareas
 
-
+#la primera clase con las definiciones desde task.py
 class Tarea:
-    def __init__(nombre, descripcion, categoria, prioridad, estado):
-        id = uuid
-        nombre = nombre
-        descripcion = descripcion
-        categoria = categoria
-        prioridad = prioridad
-        estado = estado
-        fecha_creacion = today
-        fecha_completado = None
+    def __init__(self, nombre, descripcion, categoria, prioridad, estado):
+        self.id = str(uuid.uuid4())
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.categoria = categoria
+        self.prioridad = prioridad
+        self.estado = estado
+        self.fecha_creacion = datetime.datetime.now().strftime("%d/%m/%Y")
+        self.fecha_completado = None
 
-    def completar():
-        estado = "completado"
-        fecha_completado = today
+    def completar(self):
+        self.estado = "completado"
+        self.fecha_completado = datetime.datetime.now().strftime("%d/%m/%Y")
+    
+    #esta basicamente lo convierte a dic para json
+    def to_dict(self):
+        return {
+            "ID": self.id,
+            "nombre": self.nombre,
+            "descripcion": self.descripcion,
+            "categoria": self.categoria,
+            "prioridad": self.prioridad,
+            "estado": self.estado,
+            "fecha_creacion": self.fecha_creacion,
+            "fecha_completado": self.fecha_completado
+        }
 
+class GestorTareas:
+    def __init__(self):
+        self.tareas = []
 
-    def editar(campo, nuevo_valor):
-        
+    def _lista_para_json(self):
+        resultado = []
+        for t in self.tareas:
+            resultado.append(t.to_dict())
+        return resultado
+    
+    def guardar(self):
+        guardar_tareas(self._lista_para_json())
+    
+    def agregar(self, tarea):
+        self.tareas.append(tarea)
+        self.guardar()
+     
+    def listar(self):
+        for t in self.tareas:
+            print(t.to_dict())
+    
+
+    
+    
